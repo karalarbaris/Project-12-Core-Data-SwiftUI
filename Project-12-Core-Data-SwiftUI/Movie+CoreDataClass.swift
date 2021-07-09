450c4cf114c2b6c06ebe8a375545e1a700743c2f
Baris <karalarbaris@gmail.com>
2021-07-09 13:47:56 +0200

    Creating NSManagedObject subclasses

---
+++ Project-12-Core-Data-SwiftUI/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  Project-12-Core-Data-SwiftUI
+//
+//  Created by Baris Karalar on 9.07.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
